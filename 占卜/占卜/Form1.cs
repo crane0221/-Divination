60626aafd38dc35404a5b7b6729d21dd537fd476
crane0221 <75875765+crane0221@users.noreply.github.com>
2020-12-12 14:56:39 +0800

    Add files via upload

---
+++ 占卜/占卜/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 占卜
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mm, dd;
+            int m,d,s;
+            mm = dateTimePicker1.Value.Month.ToString();
+            dd = dateTimePicker1.Value.Day.ToString();
+            m = int.Parse(mm);
+            d = int.Parse(dd);
+            s = (m * 2 + d) % 3;
+            if (s == 0)
+            {
+                richTextBox1.Text = "普通";
+            }
+            else if (s == 1)
+            {
+                richTextBox1.Text = "吉";
+            }
+            else if (s == 2)
+            {
+                richTextBox1.Text = "大吉";
+            }
+        }
+    }
+}
